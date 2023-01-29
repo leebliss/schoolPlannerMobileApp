ecb091477b403ee657f1b9092aed7fb8ceb0c942
leebliss <leebliss@yahoo.com>
2023-01-29 21:15:24 +1300

    School Planner Mobile App Final project for WGU course C196

--- app/src/androidTest/java/com/example/schoolplanner/DBHelperAndroidTest.java
+++ app/src/androidTest/java/com/example/schoolplanner/DBHelperAndroidTest.java
@@ -1,7 +1,6 @@
 package com.example.schoolplanner;
 
 import android.content.Context;
-
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
@@ -15,15 +14,31 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+public class DBHelperAndroidTest {
+
+    //this test tests that the test conifguration is correct and working
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.schoolplanner", appContext.getPackageName());
-
-
-
     }
+
+    //test to see if accurate record count is being returned
+    @Test
+    public void isRecordCountCorrect(){
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        //create object containing method to test
+        DBHelper DB = new DBHelper(appContext);
+        String tableName = "CourseInfo";
+        int termID = 1;
+        //set 'expected' to the number of records in the CourseInfo table with TermID
+        assertEquals(3, DB.getRecordCount(tableName, termID) );
+    }
+
+
+
 }
